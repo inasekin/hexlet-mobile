9fd22301829473578e3d834b4a34fa7d4b42f50e
inasekin <nasekinid8591@yandex.ru>
2024-11-13 21:27:20 +0300

    add screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,14 +29,14 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'О себе',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="programming"
         options={{
-          title: 'Explore',
+          title: 'Мои языки программирования',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
---
+++ app/(tabs)/programming.tsx
@@ -0,0 +1,33 @@
+import {useState} from "react";
+import {SafeAreaView, FlatList, StyleSheet, RefreshControl} from "react-native";
+import {programmingLanguages} from "@/components/data/data";
+import ProgrammingLanguageCard from "@/components/ProgrammingLanguageCard";
+
+export default function ProgrammingScreen() {
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = () => {
+        setRefreshing(true);
+        setTimeout(() => setRefreshing(false), 2000);
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={programmingLanguages}
+                keyExtractor={(item) => item.name}
+                renderItem={({ item }) => (
+                    <ProgrammingLanguageCard name={item.name} experience={item.experience} logo={item.logo} />
+                )}
+                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+            />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 16,
+    },
+});
